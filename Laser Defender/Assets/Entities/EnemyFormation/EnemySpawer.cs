3044a99a360901648bbd1f7693cdd7ae9325ccf8
JohnShu <fatjohn0927@gmail.com>
2017-07-27 18:13:39 +0800

    course 6-15

---
+++ Laser Defender/Assets/Entities/EnemyFormation/EnemySpawer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawer : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    public float speed = 5f;
+
+    private bool isMoveRight = false;
+    private float xmin;
+    private float xmax;
+
+    // Use this for initialization
+    void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        var leftMostPoint = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        var rightMostPoint = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xmin = leftMostPoint.x + width * 0.5f;
+        xmax = rightMostPoint.x - width * 0.5f;
+
+        foreach (Transform child in transform)
+        {
+            var enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity);
+            enemy.transform.parent = child;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = transform.position.x;
+        if (isMoveRight)
+        {
+            newX = (transform.position + Vector3.right * speed * Time.deltaTime).x;
+        }
+        else
+        {
+            newX = (transform.position + Vector3.left * speed * Time.deltaTime).x;
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(newX, xmin, xmax), transform.position.y, transform.position.z);
+
+        if (newX >= xmax || newX <= xmin)
+        {
+            isMoveRight = !isMoveRight;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+}
---
+++ Laser Defender/Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 15;
+    public float padding = 1f;
+    public GameObject projectile;
+    public float projecttileSpeed = 1;
+    public float firingRate = 0.2f;
+    public float Health = 250f;
+
+    private float xmin;
+    private float xmax;
+
+    // Use this for initialization
+    void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        var leftMostPoint = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        var rightMostPoint = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xmin = leftMostPoint.x + padding;
+        xmax = rightMostPoint.x - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", 0.000001f, firingRate);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        var newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var projectile = collision.gameObject.GetComponent<Projectile>();
+
+        if (projectile == null)
+        {
+            return;
+        }
+
+        Health -= projectile.Damage;
+
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Fire()
+    {
+        var startPosition = transform.position + new Vector3(0, 1f, 0);
+        var laserBeam = Instantiate(projectile, startPosition, Quaternion.identity);
+        laserBeam.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projecttileSpeed);
+    }
+}
